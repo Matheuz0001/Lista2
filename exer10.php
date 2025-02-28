11fd96dfb788ff637ef670e2df9a88765c7a1739
garoto_de_programa <matheuz.lima0814@gmail.com>
2025-02-28 14:23:20 -0300

    exercicio10

---
+++ exer10.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 10</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1>Exercicio 10</h1>
+
+        <form method="post" action="respostaexer10.php">
+
+            <div class="mb-3">
+                <label for="lado1" class="form-label">Valor lado 1</label>
+                <input type="number" id="lado1" name="lado1" class="form-control" required="">
+            </div>
+
+            <div class="mb-3">
+                <label for="lado2" class="form-label">Valor lado 2</label>
+                <input type="number" id="lado2" name="lado2" class="form-control" required="">
+            </div>
+
+            <div class="mb-3">
+                <label for="lado3" class="form-label">Valor lado 3</label>
+                <input type="number" id="lado3" name="lado3" class="form-control" required="">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
