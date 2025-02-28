08857ef65f23b63e93df5dcf79a5355b4e0ec1c6
garoto_de_programa <matheuz.lima0814@gmail.com>
2025-02-28 10:56:06 -0300

    Atualização + Exercicio 3

--- exer3.php
+++ exer3.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 2</title>
+    <title>Exercicio 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container mt-5">
-        <h1>Exercicio 2</h1>
+        <h1>Exercicio 3</h1>
 
-        <form method="post" action="respostaexer2.php">
+        <form method="post" action="respostaexer3.php">
 
             <div class="mb-3">
                 <label for="valor1" class="form-label">Valor 1</label>
--- respostaexer3.php
+++ respostaexer3.php
@@ -4,28 +4,28 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 2</title>
+    <title>Resposta exercicio 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container mt-5">
-        <h1>Exercicio 2</h1>
+        <h1>Resposta exercicio 3</h1>
 
-        <form method="post" action="respostaexer2.php">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            try {
+                $valor1 = $_POST['valor1'];
+                $valor2 = $_POST['valor2'];
+                $sub = $valor1 * $valor2;
+                echo "O valor da subtração é: $sub";
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        }
 
-            <div class="mb-3">
-                <label for="valor1" class="form-label">Valor 1</label>
-                <input type="number" id="valor1" name="valor1" class="form-control" required="">
-            </div>
 
-            <div class="mb-3">
-                <label for="valor2" class="form-label">Valor 2</label>
-                <input type="number" id="valor2" name="valor2" class="form-control" required="">
-            </div>
-
-            <button type="submit" class="btn btn-primary">Enviar</button>
-        </form>
+        ?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </div>
